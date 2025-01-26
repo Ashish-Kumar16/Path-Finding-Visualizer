58817fb4fafbb63f3085157f53e4562dd83f184b
Ashish-0016 <ashish.as6021@gmail.com>
2025-01-26 15:04:21 +0530

    updated whole project used reactjs DSA

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// App.jsx
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PathVisualizer from "./components/PathVisualizer/PathVisualizer";
+
+const App = () => {
+  return <PathVisualizer />;
+};
+
+export default App;
